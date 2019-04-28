e09be138d76588c72c713ad0d47ecb5845dae9de
robbbowes <up666724@myport.ac.uk>
2019-04-28 10:47:19 +0200

    Game logic working

--- app/src/main/java/com/example/connectthree/MainActivity.java
+++ app/src/main/java/com/example/connectthree/MainActivity.java
@@ -10,22 +10,41 @@ public class MainActivity extends AppCompatActivity {
 
     // 1 = red, 2 = yellow, 0 = empty
     private int activePlayer = 1;
-    private boolean gameWon = false;
     private int[] gameState = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-
+    private int[][] winningCombos = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    private int winner = 0;
 
     public void clickGrid(View view) {
-        ImageView imageView = (ImageView) view;
-        imageView.setTranslationY(-1500);
+        if (winner == 0) {
+            ImageView imageView = (ImageView) view;
+            imageView.setTranslationY(-1500);
 
-        int clickedBox = Integer.parseInt(imageView.getTag().toString());
-        gameState[clickedBox] = activePlayer;
+            int clickedBox = Integer.parseInt(imageView.getTag().toString());
+            gameState[clickedBox] = activePlayer;
 
-        int drawable = activePlayer == 1 ? R.drawable.red : R.drawable.yellow;
-        imageView.setImageResource(drawable);
-        imageView.animate().translationYBy(1500).rotation(1080).setDuration(300);
+            int drawable = activePlayer == 1 ? R.drawable.red : R.drawable.yellow;
+            imageView.setImageResource(drawable);
+            imageView.animate().translationYBy(1500).rotation(1080).setDuration(300);
+            winnerChecker();
+            if (winner != 0) {
+                System.out.println(activePlayer + " has won!!");
+            } else {
+                activePlayer = activePlayer == 1 ? 2 : 1;
+            }
+        }
+    }
 
-        activePlayer = activePlayer == 1 ? 2 : 1;
+    private void winnerChecker() {
+        for (int[] winningCombo : winningCombos) {
+            if (
+                    (gameState[winningCombo[0]] == gameState[winningCombo[1]])
+                    && (gameState[winningCombo[1]] == gameState[winningCombo[2]])
+                    && (gameState[winningCombo[0]] != 0)
+            ) {
+                winner = activePlayer;
+                break;
+            }
+        }
     }
 
     @Override
